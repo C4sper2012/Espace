19343887d687ba88337d27f750a9815159b28861
Casper <casper.koch.dev@gmail.com>
2022-02-15 19:48:56 +0100

    added update to ui

---
+++ Espace.RazorPage/Pages/Details.cshtml.cs
@@ -0,0 +1,25 @@
+﻿using Espace.Service.Helpers;
+using Espace.Service.Shared.Contracts;
+using Espace.Service.Shared.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Espace.RazorPage.Pages
+{
+    public class DetailsModel : PageModel
+    {
+        private ITodoService _service;
+
+        public DetailsModel(ITodoService service)
+        {
+            _service = service;
+        }
+
+        [BindProperty] public TodoItem TodoItem { get; set; } = new();
+
+        public async Task OnGetAsync(int id)
+        {
+            TodoItem = await _service.GetItemByIdAsync(id);
+        }
+    }
+}
